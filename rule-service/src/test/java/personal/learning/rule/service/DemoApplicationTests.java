6309ce53d3e11823c50e83b237befd2d599bb50b
Ritresh Girdhar <ritgirdh@publicisgroupe.net>
2021-02-20 21:51:11 +0530

    Drools rule service

---
+++ rule-service/src/test/java/personal/learning/rule/service/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.learning.rule.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
